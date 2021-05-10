69e4745956b97610b26aa74f403c6af16e610216
Jay Shepherd <jay@sprucecreeksoftware.com>
2021-05-10 11:09:44 -0500

    Add with_message for dimension_validator_matcher

--- test/dummy/app/models/project.rb
+++ test/dummy/app/models/project.rb
@@ -15,6 +15,7 @@ class Project < ApplicationRecord
   has_one_attached :attachment
   has_one_attached :small_file
   has_one_attached :dimension_exact
+  has_one_attached :dimension_exact_with_message
   has_one_attached :dimension_range
   has_one_attached :dimension_min
   has_one_attached :dimension_max
@@ -28,9 +29,10 @@ class Project < ApplicationRecord
   validates :small_file, attached: true, size: { less_than: 1.kilobytes }
   validates :documents, limit: { min: 1, max: 3 }
 
-  validates :dimension_exact,  dimension: { width: 150, height: 150 }
-  validates :dimension_range,  dimension: { width: { in: 800..1200 }, height: { in: 600..900 } }
-  validates :dimension_min,    dimension: { min: 800..600 }
-  validates :dimension_max,    dimension: { max: 1200..900 }
-  validates :dimension_images, dimension: { width: { min: 800, max: 1200 }, height: { min: 600, max: 900 } }
+  validates :dimension_exact,               dimension: { width: 150, height: 150 }
+  validates :dimension_exact_with_message,  dimension: { width: 150, height: 150, message: 'Invalid dimensions.' }
+  validates :dimension_range,               dimension: { width: { in: 800..1200 }, height: { in: 600..900 } }
+  validates :dimension_min,                 dimension: { min: 800..600 }
+  validates :dimension_max,                 dimension: { max: 1200..900 }
+  validates :dimension_images,              dimension: { width: { min: 800, max: 1200 }, height: { min: 600, max: 900 } }
 end
