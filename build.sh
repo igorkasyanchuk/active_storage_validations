8cd2eadc7aa67359cbca51ef05a8f96037f8a39a
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2021-06-26 00:55:11 -0700

    new release preparation

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+BUNDLE_GEMFILE=gemfiles/rails_next.gemfile bundle
+BUNDLE_GEMFILE=gemfiles/rails_6_1.gemfile bundle
+BUNDLE_GEMFILE=gemfiles/rails_6_0.gemfile bundle
+BUNDLE_GEMFILE=gemfiles/rails_5_2.gemfile bundle
 rm *.gem
 rm -fr test/dummy/log/*.log
 rm -fr test/dummy/tmp/cache
