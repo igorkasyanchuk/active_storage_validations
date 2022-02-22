22c33b2471e070980ae1a907073a71d9bdd66d96
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-02-22 19:18:59 +0100

    Remove references to image/jpg content type
    This content type raises a deprecation warning on Rails 7.0
    
    DEPRECATION WARNING: image/jpg is not a valid content type, it should
    not be used when creating a blob, and support for it will be removed
    in Rails 7.1. If you want to keep supporting this content type past
    Rails 7.1, add it to `config.active_storage.variable_content_types`.
    
    https://www.w3.org/Graphics/JPEG/

--- test/dummy/app/models/only_image.rb
+++ test/dummy/app/models/only_image.rb
@@ -2,5 +2,5 @@ class OnlyImage < ApplicationRecord
   has_one_attached :image
   validates :image, dimension: { width: { min: 100, max: 2000 }, height: { min: 100, max: 1500 } },
                     aspect_ratio: :is_16_9,
-                    content_type: ['image/png', 'image/jpg']
+                    content_type: ['image/png', 'image/jpeg']
 end
