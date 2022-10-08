8cb152b7a1f7a4dee4c4ab4e8e53970682aa49d2
Niklas Bichinger <niklas@bichinger.de>
2022-10-08 17:51:54 +0200

    add tests for #166

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many_attached :photos
   has_one_attached :image_regex
   has_one_attached :conditional_image
+  has_one_attached :conditional_image_2
   has_one_attached :proc_avatar
   has_many_attached :proc_photos
   has_one_attached :proc_image_regex
@@ -23,6 +24,7 @@ class User < ApplicationRecord
   validates :photos, attached: true, content_type: ['image/png', 'image/jpg', /\A.*\/pdf\z/]
   validates :image_regex, content_type: /\Aimage\/.*\z/
   validates :conditional_image, attached: true, if: -> { name == 'Foo' }
+  validates :conditional_image_2, attached: true, content_type: -> (record) {[/\Aimage\/.*\z/]}, size: { less_than: 10.megabytes }, if: -> { name == 'Peter Griffin' }
   validates :proc_avatar, attached: { message: "must not be blank" }, content_type: -> (record) {:png}
   validates :proc_photos, attached: true, content_type: -> (record) {['image/png', 'image/jpg', /\A.*\/pdf\z/]}
   validates :proc_image_regex, content_type: -> (record) {/\Aimage\/.*\z/}
