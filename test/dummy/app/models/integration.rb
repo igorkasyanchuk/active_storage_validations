9872d4cd1e1868ca600f7d7552a1e017d295a2fd
Mth0158 <mathieu.eustachy@gmail.com>
2023-11-06 20:08:13 +0100

    [Test] Add our first matcher integration test

---
+++ test/dummy/app/models/integration.rb
@@ -0,0 +1,5 @@
+module Integration
+  def self.table_name_prefix
+    'integration_'
+  end
+end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define do
     t.datetime :updated_at, precision: 6, null: false
   end
 
+  create_table :integration_matchers, force: :cascade do |t|
+    t.datetime :created_at, null: false
+    t.datetime :updated_at, null: false
+  end
+
   create_table :limit_attachments, force: :cascade do |t|
     t.string :name
   end
