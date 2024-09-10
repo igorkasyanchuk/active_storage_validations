3729a2f60df037cada3566847f5fbf9b6a90ac1b
Titien OREFICE <167072463+Scorpahr@users.noreply.github.com>
2024-09-10 15:19:04 +0200

    [Matcher] Add processable_image validator matcher (#262)

--- lib/active_storage_validations/matchers/concerns/attachable.rb
+++ lib/active_storage_validations/matchers/concerns/attachable.rb
@@ -3,8 +3,8 @@ module ActiveStorageValidations
     module Attachable
       private
 
-      def attach_file
-        @subject.public_send(@attribute_name).attach(dummy_file)
+      def attach_file(file = dummy_file)
+        @subject.public_send(@attribute_name).attach(file)
         @subject.public_send(@attribute_name)
       end
 
@@ -16,12 +16,32 @@ module ActiveStorageValidations
         }
       end
 
+      def processable_image
+        {
+          io: File.open(Rails.root.join('public', 'image_1920x1080.png')),
+          filename: 'image_1920x1080_file.png',
+          content_type: 'image/png'
+        }
+      end
+
+      def not_processable_image
+        {
+          io: Tempfile.new('.'),
+          filename: 'processable.txt',
+          content_type: 'text/plain'
+        }
+      end
+
       def io
         @io ||= Tempfile.new('Hello world!')
       end
 
       def detach_file
         @subject.attachment_changes.delete(@attribute_name.to_s)
+      end
+
+      def file_attached?
+        @subject.public_send(@attribute_name).attached?
       end
     end
   end
--- lib/active_storage_validations/matchers/processable_image_validator_matcher.rb
+++ lib/active_storage_validations/matchers/processable_image_validator_matcher.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'concerns/active_storageable.rb'
+require_relative 'concerns/allow_blankable.rb'
 require_relative 'concerns/attachable.rb'
 require_relative 'concerns/contextable.rb'
 require_relative 'concerns/messageable.rb'
@@ -9,12 +10,13 @@ require_relative 'concerns/validatable.rb'
 
 module ActiveStorageValidations
   module Matchers
-    def validate_attached_of(attribute_name)
-      AttachedValidatorMatcher.new(attribute_name)
+    def validate_processable_image_of(name)
+      ProcessableImageValidatorMatcher.new(name)
     end
 
-    class AttachedValidatorMatcher
+    class ProcessableImageValidatorMatcher
       include ActiveStorageable
+      include AllowBlankable
       include Attachable
       include Contextable
       include Messageable
@@ -22,6 +24,7 @@ module ActiveStorageValidations
       include Validatable
 
       def initialize(attribute_name)
+        initialize_allow_blankable
         initialize_contextable
         initialize_messageable
         initialize_rspecable
@@ -29,53 +32,46 @@ module ActiveStorageValidations
       end
 
       def description
-        "validate that :#{@attribute_name} must be attached"
+        "validate that :#{@attribute_name} is a processable image"
       end
 
       def failure_message
-        "is expected to validate attachment of :#{@attribute_name}"
+        "is expected to validate the processable image of :#{@attribute_name}"
       end
 
       def matches?(subject)
         @subject = subject.is_a?(Class) ? subject.new : subject
 
         is_a_valid_active_storage_attribute? &&
-          is_context_valid? &&
-          is_custom_message_valid? &&
-          is_valid_when_file_attached? &&
-          is_invalid_when_file_not_attached?
+        is_context_valid? &&
+        is_custom_message_valid? &&
+        is_valid_when_image_processable? &&
+        is_invalid_when_image_not_processable?
       end
 
       private
 
-      def is_valid_when_file_attached?
-        attach_file unless file_attached?
+      def is_valid_when_image_processable?
+        attach_file(processable_image)
         validate
+        detach_file
         is_valid?
       end
 
-      def is_invalid_when_file_not_attached?
-        detach_file if file_attached?
+      def is_invalid_when_image_not_processable?
+        attach_file(not_processable_image)
         validate
+        detach_file
         !is_valid?
       end
 
       def is_custom_message_valid?
         return true unless @custom_message
 
-        detach_file if file_attached?
+        attach_file(not_processable_image)
         validate
+        detach_file
         has_an_error_message_which_is_custom_message?
-      end
-
-      def file_attached?
-        @subject.public_send(@attribute_name).attached?
-      end
-
-      def detach_file
-        @subject.public_send(@attribute_name).detach
-        # Unset the direct relation since `detach` on an unpersisted record does not set `attached?` to false.
-        @subject.public_send("#{@attribute_name}=", nil)
       end
     end
   end
