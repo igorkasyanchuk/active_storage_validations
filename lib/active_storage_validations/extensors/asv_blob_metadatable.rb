9fc06d914c43cddbb0890eb86840c6251b5ec3b3
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2025-01-28 15:04:38 +0100

    [Metadata] Flatten metadata to asv_xxx instead of hash for making it settable for services metadata (#348)

--- lib/active_storage_validations/extensors/asv_blob_metadatable.rb
+++ lib/active_storage_validations/extensors/asv_blob_metadatable.rb
@@ -5,19 +5,23 @@ module ActiveStorageValidations
     extend ActiveSupport::Concern
 
     included do
+      # This method returns the metadata that has been set by our gem.
+      # The metadata is stored in the blob's custom metadata. All keys are prefixed with 'asv_'
+      # to avoid conflicts with other metadata.
+      # It is not to set a active_storage_validation key equal to a a hash of our gem's metadata,
+      # because this would result in errors down the road with services such as S3.
       def active_storage_validations_metadata
-        metadata.dig('custom', 'active_storage_validations') || {}
+        metadata.dig('custom')
+                &.select { |key, _| key.to_s.start_with?('asv_') }
+                &.transform_keys { |key| key.to_s.delete_prefix('asv_') } || {}
       end
 
-      def active_storage_validations_metadata=(value)
+      # This method sets the metadata that has been detected by our gem.
+      # The metadata is stored in the blob's custom metadata. All keys are prefixed with 'asv_'.
+      def merge_into_active_storage_validations_metadata(hash)
         metadata['custom'] ||= {}
-        metadata['custom']['active_storage_validations'] = value
-      end
-
-      def merge_into_active_storage_validations_metadata(new_data)
-        metadata['custom'] ||= {}
-        metadata['custom']['active_storage_validations'] ||= {}
-        metadata['custom']['active_storage_validations'].merge!(new_data)
+        metadata['custom'].merge!(hash.transform_keys { |key, _| key.to_s.start_with?('asv_') ? key : "asv_#{key}" })
+        active_storage_validations_metadata
       end
     end
   end
--- test/extensors/asv_blob_metadatable_test.rb
+++ test/extensors/asv_blob_metadatable_test.rb
@@ -11,18 +11,13 @@ describe ActiveStorageValidations::ASVBlobMetadatable do
     end
   end
 
-  describe "#active_storage_validations_metadata=" do
-    it "adds our gem's setter method to ActiveStorage::Blob custom metadata" do
-      blob.active_storage_validations_metadata = { 'duration' => '1.0' }
-      assert blob.active_storage_validations_metadata == { 'duration' => '1.0' }
-    end
-  end
-
   describe "#merge_into_active_storage_validations_metadata" do
     it "adds our gem's setter method to ActiveStorage::Blob custom metadata" do
-      blob.active_storage_validations_metadata = { 'duration' => '1.0' }
+      blob.custom_metadata = { 'asv_duration' => '1.0' }
       blob.merge_into_active_storage_validations_metadata({ 'duration' => '2.0', 'audio' => false })
+
       assert blob.active_storage_validations_metadata == { 'duration' => '2.0', 'audio' => false }
+      assert blob.custom_metadata == { 'asv_duration' => '2.0', 'asv_audio' => false }
     end
   end
 end
