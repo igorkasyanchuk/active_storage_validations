f2371910fb95a702939b5688ca5a2c85c25bac44
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2025-04-12 19:11:30 +0200

    [Test] Fix test issue for Rails < 7.0

--- test/extensors/asv_blob_metadatable_test.rb
+++ test/extensors/asv_blob_metadatable_test.rb
@@ -12,13 +12,15 @@ describe ActiveStorageValidations::ASVBlobMetadatable do
   end
 
   describe "#merge_into_active_storage_validations_metadata" do
-    it "adds our gem's setter method to ActiveStorage::Blob custom metadata" do
+    before do
       if Rails.gem_version >= Gem::Version.new("7.0.0.rc1")
         blob.custom_metadata = { "asv_duration" => "1.0", "asv_width" => "100" }
       else
         blob.metadata[:custom] = { "asv_duration" => "1.0", "asv_width" => "100" }
       end
+    end
 
+    it "adds our gem's setter method to ActiveStorage::Blob custom metadata" do
       blob.merge_into_active_storage_validations_metadata({ "duration" => 2.0, "audio" => false })
 
       assert blob.active_storage_validations_metadata == { "duration" => 2.0, "audio" => false, "width" => 100 }
@@ -33,7 +35,13 @@ describe ActiveStorageValidations::ASVBlobMetadatable do
 
   describe "#remove_active_storage_validations_metadata!" do
     describe "when the blob has our gem's metadata" do
-      before { blob.custom_metadata = { "asv_duration" => "1.0", "asv_width" => "100" } }
+      before do
+        if Rails.gem_version >= Gem::Version.new("7.0.0.rc1")
+          blob.custom_metadata = { "asv_duration" => "1.0", "asv_width" => "100" }
+        else
+          blob.metadata[:custom] = { "asv_duration" => "1.0", "asv_width" => "100" }
+        end
+      end
 
       it "removes our gem's metadata from ActiveStorage::Blob custom metadata" do
         blob.remove_active_storage_validations_metadata!
