62f4caedb4298b4d55685cf87a4a28cd2a84381a
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2025-05-27 11:23:49 +0200

    update"

--- lib/ruby_lsp/active_storage_validations/hover.rb
+++ lib/ruby_lsp/active_storage_validations/hover.rb
@@ -1,26 +1,24 @@
-module RubyLsp
-  module ActiveStorageValidations
-    class Hover
-      include RubyLsp::Requests::Support::Common
+class Hover
+  include RubyLsp::Requests::Support::Common
 
-      def initialize(response_builder, dispatcher)
-        @response_builder = response_builder
-        dispatcher.register(self, :symbol_node)
-      end
+  def initialize(response_builder, dispatcher)
+    @response_builder = response_builder
+    dispatcher.register(self, :pair_node)
+  end
 
-      def on_symbol_node(node)
-        if node.value == :size
-          @response_builder.push(
-            Prism::Location.new(node.location.start_offset, node.location.end_offset),
-            {
-              kind: "markdown",
-              value: "**`size` (ActiveStorage Validations)**\n\n" \
-                     "Validates the size of an attached file.\n\n" \
-                     "[Documentation](https://github.com/rails/active_storage_validations#size)"
-            }
-          )
-        end
-      end
-    end
+  def on_pair_node(node)
+    key_node = node.key
+
+    return unless key_node.type == :label && key_node.value == "size"
+
+    @response_builder.push(
+      Prism::Location.new(key_node.location.start_offset, key_node.location.end_offset),
+      {
+        kind: "markdown",
+        value: "**`size` validator (ActiveStorage Validations)**\n\n" \
+               "Validates the size of an attached file or image.\n\n" \
+               "[📘 Documentation](https://github.com/rails/active_storage_validations#size)"
+      }
+    )
   end
 end
